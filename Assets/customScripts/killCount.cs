67fea9f422176267f6c565b5d412752f5b49959e
AndyZYZ <onezezhang@gmail.com>
2014-02-07 21:48:16 +0800

    add player and AI enemy
    there are still a lot of bugs need to be fixed

---
+++ Assets/customScripts/killCount.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killCount : MonoBehaviour {
+	public int turretKill;
+	// Use this for initialization
+	void Start () {
+		turretKill = 0;
+	}
+	public void addKcount() {
+		turretKill++;
+	}
+	public int getKcount() {
+		return turretKill;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
